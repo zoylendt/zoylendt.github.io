af7a004f854b0f8cd8ab09fc2d12940ed74e088d
zoylendt <48225236+zoylendt@users.noreply.github.com>
2024-05-11 04:34:00 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🍺 ZoyBlog 📚",
+    pageTitle: "🍺ZoyBlog📚",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
