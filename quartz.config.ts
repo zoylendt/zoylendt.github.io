720721a6c42b0d676997587a944c84da3cfcf56b
zoylendt <48225236+zoylendt@users.noreply.github.com>
2024-05-11 04:23:33 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Bebas Neue",
+        header: "Cinzel",
         body: "Lexend",
         code: "JetBrains Mono",
       },
