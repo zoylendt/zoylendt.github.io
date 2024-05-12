96ddeae9d31802d931a4eee9498182d1420b6860
zoylendt <48225236+zoylendt@users.noreply.github.com>
2024-05-12 03:17:32 +0200

    Update LinksHeader.tsx

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -11,28 +11,28 @@ export default (() => {
       <div>
         <div id="links-header">
           <span>
-            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Card%20index/Color/card_index_color.svg"></img>
-            <a href="https://camargomau.com/">Blog</a>
-          </span>
-          <span>
-            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Books/Color/books_color.svg"></img>
-            <a href="/Sciujo/MAC/MAC">MAC</a>
-          </span>
-          <span>
             <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Brain/Color/brain_color.svg"></img>
-            <a href="/Sciujo/Sciujo">Learn</a>
+            <a href="/Braindump">Brain</a>
           </span>
           <span>
-            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Parrot/Color/parrot_color.svg"></img>
-            <a href="/Lingvujo/Lingvujo">Langs</a>
+            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Keyboard/Color/keyboard_color.svg"></img>
+            <a href="/Coding">Code</a>
           </span>
           <span>
-            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Triangular%20ruler/Color/triangular_ruler_color.svg"></img>
-            <a href="/Sciujo/Mathematics">Maths</a>
+            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Beer%20mug/Color/beer_mug_color.svg"></img>
+            <a href="/Brewing">Brew</a>
           </span>
           <span>
-            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Laptop/Color/laptop_color.svg"></img>
-            <a href="/Sciujo/Computing">Comp</a>
+            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Atom%20symbol/Color/atom_symbol_color.svg"></img>
+            <a href="/Physics">Physics</a>
+          </span>
+          <span>
+            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Open%20book/Color/open_book_color.svg"></img>
+            <a href="/Media">Media</a>
+          </span>
+          <span>
+            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Spouting%20whale/Color/spouting_whale_color.svg"></img>
+            <a href="/Selfhosted">Selfhosted</a>
           </span>
         </div>
       <hr style="background-color: var(--gray); border-top: 1px var(--gray) solid; margin-top: 1.3rem"></hr>
