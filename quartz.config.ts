29114b9da623b73ca901f12ad99edda6eb361473
zoylendt <48225236+zoylendt@users.noreply.github.com>
2024-12-04 20:49:03 +0100

    removed emoji from end of page title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🍺ZoyBlog📚",
+    pageTitle: "🍺ZoyBlog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
