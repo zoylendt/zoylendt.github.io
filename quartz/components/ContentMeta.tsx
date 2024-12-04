49b27e81febe7674e1406f43b54a9fa39d9a6b0c
zoylendt <48225236+zoylendt@users.noreply.github.com>
2024-12-04 03:48:45 +0100

    try 1

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -27,13 +27,27 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
     const text = fileData.text
 
     if (text) {
+      var modifiedSegment: string = ""
+      var createdSegment: string = ""
+      const fileRelativePath = fileData.filePath
       const segments: (string | JSX.Element)[] = []
 
       if (fileData.dates) {
         segments.push(<Date date={getDate(cfg, fileData)!} locale={cfg.locale} />)
+
+        if (fileData.dates.created) {
+          cfg.defaultDateType = "created"
+          createdSegment = formatDate(getDate(cfg, fileData)!)
+        }
+
+        if (fileData.dates.modified) {
+          cfg.defaultDateType = "modified"
+          modifiedSegment = formatDate(getDate(cfg, fileData)!)
+        }
       }
 
       // Display reading time if enabled
+      var readingTimeStr: string = ""
       if (options.showReadingTime) {
         const { minutes, words: _words } = readingTime(text)
         const displayedTime = i18n(cfg.locale).components.contentMeta.readingTime({
@@ -43,8 +57,18 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
       }
 
       return (
-        <p show-comma={options.showComma} class={classNames(displayClass, "content-meta")}>
-          {segments}
+        <p class={classNames(displayClass, "content-meta")}>
+          {readingTimeStr} <br />
+          Created {createdSegment} & updated {modifiedSegment} <br /> 
+          🌟 <a href={`https://github.com/zoylendt/zoylendt.github.io/blob/v4/${fileRelativePath}?plain=1`} class={classNames(displayClass, "external")} target={"_blank"} style={"font-weight:400"}>
+            Code<svg class="external-icon" viewBox="0 0 512 512"><path d="M320 0H288V64h32 82.7L201.4 265.4 178.7 288 224 333.3l22.6-22.6L448 109.3V192v32h64V192 32 0H480 320zM32 32H0V64 480v32H32 456h32V480 352 320H424v32 96H64V96h96 32V32H160 32z"></path></svg>
+          </a> &nbsp;
+          📄 <a href={`https://github.com/zoylendt/zoylendt.github.io/raw/v4/${fileRelativePath}`} class={classNames(displayClass, "external")} target={"_blank"} style={"font-weight:400"}>
+            Raw .md<svg class="external-icon" viewBox="0 0 512 512"><path d="M320 0H288V64h32 82.7L201.4 265.4 178.7 288 224 333.3l22.6-22.6L448 109.3V192v32h64V192 32 0H480 320zM32 32H0V64 480v32H32 456h32V480 352 320H424v32 96H64V96h96 32V32H160 32z"></path></svg>
+          </a> &nbsp;
+          🗓️ <a href={`https://github.com/zoylendt/zoylendt.github.io/commits/v4/${fileRelativePath}`} class={classNames(displayClass, "external")} target={"_blank"} style={"font-weight:400"}>
+            History<svg class="external-icon" viewBox="0 0 512 512"><path d="M320 0H288V64h32 82.7L201.4 265.4 178.7 288 224 333.3l22.6-22.6L448 109.3V192v32h64V192 32 0H480 320zM32 32H0V64 480v32H32 456h32V480 352 320H424v32 96H64V96h96 32V32H160 32z"></path></svg>
+          </a>
         </p>
       )
     } else {
